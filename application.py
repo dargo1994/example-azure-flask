8eb07b33fc20380a1fb643e17ea4a366b29b374d
dargo1994 <101544385+dargo1994@users.noreply.github.com>
2022-03-14 18:12:40 +0800

    Update application.py

--- application.py
+++ application.py
@@ -2,7 +2,7 @@ from flask import Flask, request, abort
 app = Flask(__name__)
 
 
-@app.route("/webhook", methods='[POST]')
+@app.route("/webhook", methods=["POST"])
 def webhook():
     if request.method == 'POST':
         print(request.json)

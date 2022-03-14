290cf4ab8ccd676850e602bc5ce58af7f369be93
dargo1994 <101544385+dargo1994@users.noreply.github.com>
2022-03-14 16:53:36 +0800

    Update application.py

--- application.py
+++ application.py
@@ -7,8 +7,10 @@ def webhook():
     if request.method == 'POST':
         print(request.json)
         return 'success', 200
-    else abort 
-        abort(400)
+
+@app.route("/")
+def hello():
+    return 'hello! test'
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)

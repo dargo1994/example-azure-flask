96387e25514e68c2d8c477dafb21f1d70882d7c9
dargo1994 <101544385+dargo1994@users.noreply.github.com>
2022-03-14 16:47:25 +0800

    Update application.py

--- application.py
+++ application.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, request, abort
 app = Flask(__name__)
 
 
-@app.route("/")
-def hello():
-    return "Hello, SmartNinja!"
-
+@app.route("/webhook")
+def webhook():
+    if request.method == 'POST':
+        print(request.json)
+        return 'success', 200
+    else abort 
+        abort(400)
 
 if __name__ == '__main__':
     app.run(host='127.0.0.1', port=8080, debug=True)
